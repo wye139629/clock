f63405bcc4af49b550451b8c7b13e5120b316413
Guy Lin Chien Li <guychienll@gmail.com>
2020-04-12 15:56:17 +0800

    html new blank line & background color -> none

--- clock.js
+++ clock.js
@@ -1,17 +1,15 @@
 var se;
 var min;
 var hour;
-function init(){
-  window.setInterval(countdown,1000); se=document.getElementById("se");
-  min=document.getElementById("min");
-  hour=document.getElementById("hour");
-
+function init() {
+  window.setInterval(countdown, 1000);
+  se = document.getElementById("se");
+  min = document.getElementById("min");
+  hour = document.getElementById("hour");
 }
-function countdown(){
-  se.innerHTML=se.innerHTML-(-1);
-  if(se.innerHTML==60){
-    se.innerHtml="0"   
-    
+function countdown() {
+  se.innerHTML = se.innerHTML - -1;
+  if (se.innerHTML == 60) {
+    se.innerHtml = "0";
   }
- 
-  }
+}
