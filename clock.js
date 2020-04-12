90169db3bfb8fa1c051694cc7c1ca941181212f7
will Liu <wye139629@gmail.com>
2020-04-12 15:24:37 +0800

    initial commit

---
+++ clock.js
@@ -0,0 +1,17 @@
+var se;
+var min;
+var hour;
+function init(){
+  window.setInterval(countdown,1000); se=document.getElementById("se");
+  min=document.getElementById("min");
+  hour=document.getElementById("hour");
+
+}
+function countdown(){
+  se.innerHTML=se.innerHTML-(-1);
+  if(se.innerHTML==60){
+    se.innerHtml="0"   
+    
+  }
+ 
+  }
